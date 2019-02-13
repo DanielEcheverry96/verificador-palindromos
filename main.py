1664df26e3c618a17de81ee9cb87db3e63641e3c
DanielEcheverry96 <dfecheverryg@unal.edu.co>
2019-02-12 20:49:55 -0500

    Se crean elementos de prueba

---
+++ main.py
@@ -0,0 +1,8 @@
+from verificador_palindromo import verificadorPalindromo
+
+print(verificadorPalindromo('radar'))
+print(verificadorPalindromo('jajdfas'))
+print(verificadorPalindromo('reconocer'))
+print(verificadorPalindromo('recoocer'))
+print(verificadorPalindromo('recnocer'))
+print(verificadorPalindromo('oso'))
